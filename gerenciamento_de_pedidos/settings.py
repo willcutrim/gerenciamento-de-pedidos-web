b8234c0964a8b03b38484e8e6770f8dd71126034
willcutrim <willyam894@gmail.com>
2023-01-02 20:14:24 -0300

    add: pastas de arquivos staticos

--- gerenciamento_de_pedidos/settings.py
+++ gerenciamento_de_pedidos/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    
+    # os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
