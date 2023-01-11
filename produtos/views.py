2d0cdad59246de33e66fb737d6d75d89aa03c79b
willcutrim <willyam894@gmail.com>
2023-01-11 20:59:57 -0300

    melhoria no cadastro de adição do logout

--- produtos/views.py
+++ produtos/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from .models import Produtos, Categoria
-
+from django.contrib.auth.models import User
 
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
+@login_required(login_url="/usuario/login/")
 def index_pro(request):
     categoria = Categoria.objects.filter(id=2)
-    
+    user = request.user
     for p in categoria:
         pass
     produtos = Produtos.objects.filter(categoria=p.pk)
@@ -25,7 +26,8 @@ def index_pro(request):
         'categoria_refrigerante': categoria_refrigerante, 
         'categoria_produto': categoria_produto, 
         'sucos': sucos, 
-        'categoria_suco': categoria_suco
+        'categoria_suco': categoria_suco,
+        'user': user
     }
 
     return render(request, 'html/home.html', context)
