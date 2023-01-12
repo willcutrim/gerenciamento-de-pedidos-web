864b0b9227557f687b600b90f8342b70fb145f22
willcutrim <willyam894@gmail.com>
2023-01-12 16:46:11 -0300

    melhorias no endpoint de login e token

--- pedidos/apiGetControllerPedidos.py
+++ pedidos/apiGetControllerPedidos.py
@@ -6,9 +6,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from pedidos.models import Pedidos
 from pedidos.serializers import PedidosSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 class PedidosList(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         pedidos = Pedidos.objects.all()
         serializer = PedidosSerializer(pedidos, many=True)
---
+++ usuario/api_login.py
@@ -0,0 +1,38 @@
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.request import Request
+from .tokens import create_jwt_pair_for_user
+
+
+
+class ObterToken(APIView):
+
+    def get_user(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise Http404
+            
+    def post(self, request:Request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            tokens = create_jwt_pair_for_user(user)
+            user_name = self.get_user(username)
+            response = {'message': "Login sucesso", 'tokens': tokens, 'user': str(user_name)}
+            return Response(data=response, status=status.HTTP_200_OK)
+
+        else:
+            return Response(data={"message": "Usuário ou senha inválida."})
+
+    # def get(self, request: Request):
+    #     content = {"user": str(request.user), "auth": str(request.user.email)}
+
+    #     return Response(data=content, status=status.HTTP_200_OK)
